20ae8ffbb2d43ea87566b3deb32f299be03d05ba
zylin1998 <72535355+zylin1998@users.noreply.github.com>
2024-06-14 19:15:55 +0800

    Delete Assets/Slot&Page directory

--- Assets/Slot&Page/SettingDemo/Script/Setting/TextCollect.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "Text Collection", menuName = "Setting/Collection/Text", order = 1)]
-public class TextCollect : SettingCollect
-{
-    
-}
